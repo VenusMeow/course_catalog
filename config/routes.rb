3559143e5797afbca0d227f4dc5fdc7b104801dc
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2018-10-18 23:32:49 -0400

    add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
   resources :subjects
   resources :courses
   resources :instructors
   get 'course_pages/home'
+  get 'course_pages/init'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'course_pages#home'
+  root 'course_pages#init'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008143330) do
+ActiveRecord::Schema.define(version: 20181019031111) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20181008143330) do
     t.string "idnum"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
