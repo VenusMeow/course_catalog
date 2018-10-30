dd39f30b760a78cff5234defed5dfafcba6cb668
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2018-10-29 22:32:13 -0400

    small modify

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,20 @@ class SearchController < ApplicationController
   end
 
   def result
+
     if params[:search][:subject] != ""
       @courses = Course.where(subject_id:  params[:search][:subject]).all
-      @courses = @courses.where("name ILIKE ? ","%#{params[:search][:course].downcase}%").all
+      if Rails.env.development?
+        @courses = @courses.where("name LIKE ? ","%#{params[:search][:course].downcase}%").all
+      else
+        @courses = @courses.where("name ILIKE ? ","%#{params[:search][:course].downcase}%").all
+      end
     else
-      @courses = Course.where("name ILIKE ? ","%#{params[:search][:course].downcase}%").all
+      if Rails.env.development?
+        @courses = Course.where("name LIKE ? ","%#{params[:search][:course].downcase}%").all
+      else
+        @courses = Course.where("name ILIKE ? ","%#{params[:search][:course].downcase}%").all
+      end
     end
   end
 end
